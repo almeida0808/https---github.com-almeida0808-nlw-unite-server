5aa1970ce031f463e507d2e6bca4e9c1ccc643f5
Lucas Almeida <almeidaff08@gmail.com>
2024-04-03 11:23:31 -0300

    🚚 adicionamos um modulo para as rotas

---
+++ src/routes/createEvents.ts
@@ -0,0 +1,54 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { createSlug } from "../utils/CreateSlug";
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+
+export async function CreateEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/events",
+    {
+      schema: {
+        body: z.object({
+          // faz algumas validações pra criar um evento no banco de dados
+          title: z.string().min(4), // o title tem que ser uma string com no min 4 caracteres
+          details: z.string().nullable(), // details tem que ser uma string, porem é um campo opcional
+          maximumClients: z.number().int().positive().nullable(), // tem que ser um número, inteiro, positivo, porem é opcional
+        }),
+        response: {
+          201: z.object({
+            eventId: z.string().uuid(),
+          }),
+        },
+      },
+    },
+    async (request, response) => {
+      const { title, details, maximumClients } = request.body;
+
+      const slug = createSlug(title);
+
+      // caso não encontre um slug existente, ele retorna null
+      const eventWithSameSlug = await prisma.event.findUnique({
+        // findUnique busca um campo unico na nossa tabela
+        where: { slug },
+      });
+
+      if (eventWithSameSlug !== null) {
+        // caso a resposta seja diferente de null return esse error
+        response.status(400);
+        throw new Error(
+          "Ops.. Já existe um evento com esse nome, tente novamente."
+        );
+      }
+      const event = await prisma.event.create({
+        data: {
+          title,
+          details,
+          maximumClients,
+          slug,
+        },
+      });
+      return response.status(201).send({ eventId: event.id });
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,17 @@
 import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
-import { createSlug } from "./utils/CreateSlug";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { CreateEvent } from "./routes/createEvents";
+
 const app = fastify();
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.post("/events", async (request, response) => {
-  const createEventsSchema = z.object({
-    // faz algumas validações pra criar um evento no banco de dados
-    title: z.string().min(4), // o title tem que ser uma string com no min 4 caracteres
-    details: z.string().nullable(), // details tem que ser uma string, porem é um campo opcional
-    maximumClients: z.number().int().positive().nullable(), // tem que ser um número, inteiro, positivo, porem é opcional
-  });
-
-  const { title, details, maximumClients } = createEventsSchema.parse(
-    request.body
-  );
-  const slug = createSlug(title);
-
-  // caso não encontre um slug existente, ele retorna null
-  const eventWithSameSlug = await prisma.event.findUnique({
-    // findUnique busca um campo unico na nossa tabela
-    where: { slug },
-  });
-
-  if (eventWithSameSlug !== null) {
-    // caso a resposta seja diferente de null return esse error
-    response.status(400);
-    throw new Error(
-      "Ops.. Já existe um evento com esse nome, tente novamente."
-    );
-  }
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumClients,
-      slug,
-    },
-  });
-  return response.status(201).send({ eventId: event.id });
-});
+app.register(CreateEvent);
 
 const PORT = 4444;
 
