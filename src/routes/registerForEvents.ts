582d451ac6b1842275d9973e92c0244ae3ee7f22
almeida0808 <almeidaff08@gmail.com>
2024-04-03 21:22:10 -0300

    🐛 remove framework inutil

--- src/routes/registerForEvents.ts
+++ src/routes/registerForEvents.ts
@@ -1,8 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { string, z } from "zod";
-import { request } from "http";
-import { stdin } from "process";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function registerFromEvents(app: FastifyInstance) {
